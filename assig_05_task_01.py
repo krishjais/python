7330f6135f6522f20f686286013e42d3cdabeba3
Krish Jaiswal <115935402+krishjais@users.noreply.github.com>
2025-07-09 22:13:09 +0530

    Delete assig_05_task_01.py

--- assig_05_task_01.py
+++
@@ -1,8 +0,0 @@
-dict={"krish":80 ,"john":78 , "rohit":99}
-
-a = input("enter the name to know the marks:")
-x = dict.get(a)
-if a in dict:
-    print(f"{a}'s marks: {dict.get(a)}")
-else:
-    print("student not found")
