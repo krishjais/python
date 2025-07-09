fa0d1d39c0885e7161b1c5b63b01fe18268adbc3
Krish Jaiswal <115935402+krishjais@users.noreply.github.com>
2025-07-09 21:47:31 +0530

    Add files via upload

---
+++ assignment_1/assig_01_task_01.py
@@ -0,0 +1,7 @@
+a = int(input("enter your first no. :"))
+b = int(input("enter your second no. :"))
+
+print(f"addition: {a+b}")
+print(f"substraction: {a-b}")
+print(f"multiplication: {a*b}")
+print(f"division: {a/b}")
