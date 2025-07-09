dcfcee9f16e9972a588284179aa92ed660508db2
Krish Jaiswal <115935402+krishjais@users.noreply.github.com>
2025-07-09 21:56:02 +0530

    Delete task_01.py

--- task_01.py
+++
@@ -1,7 +0,0 @@
-a = int(input("enter your first no. :"))
-b = int(input("enter your second no. :"))
-
-print(f"addition: {a+b}")
-print(f"substraction: {a-b}")
-print(f"multiplication: {a*b}")
-print(f"division: {a/b}")
